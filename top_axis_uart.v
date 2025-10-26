cd6a1160a237bf5e7b172ac1f7672a62c7c5fd1b
KShreeni <109845020+KShreeni@users.noreply.github.com>
2025-10-26 12:10:39 +0530

    Update top_axis_uart.v

--- top_axis_uart.v
+++ top_axis_uart.v
@@ -18,3 +18,35 @@ axis_fifo_uart_tx #(.WIDTH(8),.DEPTH(8),.CLK_RATE(50000000),.BAUD(115200)) axis_
 uart_rec #(.CLK_FREQ(50000000),.BAUD(115200),.DATA_BITS(DATA_BITS)) 
 uart_rec_inst(.clk(clk),.rst(rst),.rx(uart_tx),.rx_data(rx_data),.rx_valid(rx_valid));//.parity_error(parity_err));
 endmodule
+
+module axis_master_inp #(parameter WIDTH = 8) (
+    input  wire clk,
+    input  wire rst,  
+    input  wire [WIDTH-1:0] load_data,
+
+    input  wire m_axis_ready,
+    input  wire m_axis_valid,
+   
+     output reg m_axis_valid_out,
+    output reg  [WIDTH-1:0] m_axis_data             // output data
+);
+   
+    always @(posedge clk or posedge rst) begin
+        if(rst) begin
+            m_axis_data <= 0;
+            m_axis_valid_out <= 0;
+        end
+        else begin
+           if(m_axis_valid && m_axis_ready) begin
+                m_axis_valid_out <= 1;
+                 m_axis_data <= load_data;
+           end
+           else begin
+              m_axis_valid_out <= 0;
+               m_axis_data <=  m_axis_data;
+
+          end
+
+       end
+end
+endmodule
